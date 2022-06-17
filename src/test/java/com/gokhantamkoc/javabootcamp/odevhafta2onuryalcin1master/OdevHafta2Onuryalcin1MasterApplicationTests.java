9c784fbe29be27cef21e919a35dd5865d6088ccc
onuryalcin <o.yalcin17@gmail.com>
2022-06-17 22:46:43 +0300

    second commit

---
+++ src/test/java/com/gokhantamkoc/javabootcamp/odevhafta2onuryalcin1master/OdevHafta2Onuryalcin1MasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhantamkoc.javabootcamp.odevhafta2onuryalcin1master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdevHafta2Onuryalcin1MasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
